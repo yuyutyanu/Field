8afb9a8f50a6e45502057fd5a48743b82700924f
chanu <yuyutyanu@yahoo.co.jp>
2017-11-11 21:46:08 +0900

    update

--- resources/views/assets/components/Room.vue
+++ resources/views/assets/components/Room.vue
@@ -1,15 +1,29 @@
 <template>
-    <div class="">
-        <p class="room-number">Room: #{{id}}</p>
+  <div>
 
-        <div class="md">
-            <div class="before">
-                <textarea name="" id="" cols="30" rows="10" v-model="before" @input="socket"></textarea>
-            </div>
-            <div class="after" v-html="after">
-            </div>
+    <div class="invite">
+      <transition name="fade">
+        <div class="message" v-if="isVisible">
+          <span v-if="isCopy">Copied!: {{url}} </span>
+          <span v-else>Copy to clipboard</span>
         </div>
+      </transition>
+
+      <div class="copy">
+        <button @mouseover='isVisible = !isVisible' @click="isCopy = !isCopy" @mouseout="reset">Invite url︎</button>
+      </div>
     </div>
+
+    <div class="container">
+      <div class="md">
+        <div class="before">
+          <textarea name="" id="" cols="30" rows="10" v-model="before" @input="socket"></textarea>
+        </div>
+        <div class="after" v-html="after">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,7 +32,7 @@
   const client = ws('http://localhost:3333', {}).channel('md').connect()
 
   export default {
-    props:['id'],
+    props: ['id'],
     created(){
       let _me = this
       client.joinRoom(this.id, {}, function (error, joined) {
@@ -29,77 +43,126 @@
     },
     data () {
       return {
-        before:''
+        before: '',
+        isCopy: false,
+        isVisible: false
       }
     },
     computed: {
       after(){
         return marked(this.before)
+      },
+      url(){
+        return location.href
       }
     },
-    methods:{
+    methods: {
       socket(){
         client.emit('message', this.before)
+      },
+      reset(){
+        this.isCopy = false
+        this.isVisible = false
       }
     }
   }
 </script>
 
 <style scoped>
-    .room-number {
-        position: absolute;
-        color: #fff;
-        font-weight: 600;
-        left: 20px;
-        top: 15px;
-        font-size: 16px;
-    }
 
-    .md {
-        width: 80%;
-        height: 80vh;
-        background: #fff;
-        margin: 0 auto;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        letter-spacing: -.4em;
-        border: solid 10px #DE4830;
-        box-sizing: border-box;
-        border-radius:3px;
-    }
+  .invite {
+    width: auto;
+    text-align: right;
+  }
 
-    .md .before {
-        width: 50%;
-        height: 100%;
-        display: inline-block;
-        letter-spacing: normal;
-    }
+  .message {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .message span{
+    font-size:14px;
+    color: #fff;
+  }
 
-    .md .before textarea {
-        width: 100%;
-        height: 100%;
-        resize: none;
-        font-size: 15px;
-        letter-spacing: 2px;
-        padding: 20px;
-        box-sizing: border-box;
-        background:#F7F7F7;
-        outline: none;
-        overflow-y: auto;
-        border:solid 0px;
-        border-right:solid 1px #27313D;
-    }
+  .copy {
+    display: inline-block;
+    padding: 25px;
+    right: 20px;
+  }
 
-    .md .after {
-        display: inline-block;
-        width: 50%;
-        height: 100%;
-        overflow-y: auto;
-        background:#F7F7F7;
-        padding: 20px;
-        box-sizing: border-box;
-        letter-spacing: 2px;
-    }
+  .copy button {
+    background: rgba(255, 255, 255, 0.1);
+    transition: ease .2s;
+    border: solid 0px;
+    padding: 10px 20px;
+    margin-left: 10px;
+    color: #fff;
+    outline: none;
+    font-size:14px;
+  }
+
+  .copy button:hover {
+    background: rgba(255, 255, 255, 0.2);
+  }
+
+  .container {
+    position: absolute;
+    width: 80%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .md {
+    width: 100%;
+    height: 80vh;
+    background: #fff;
+    margin: 0 auto;
+    letter-spacing: -.4em;
+    border: solid 10px #DE4830;
+    box-sizing: border-box;
+    border-radius: 3px;
+  }
+
+  .md .before {
+    width: 50%;
+    height: 100%;
+    display: inline-block;
+    letter-spacing: normal;
+  }
+
+  .md .before textarea {
+    width: 100%;
+    height: 100%;
+    resize: none;
+    font-size: 15px;
+    letter-spacing: 2px;
+    padding: 20px;
+    box-sizing: border-box;
+    background: #F7F7F7;
+    outline: none;
+    overflow-y: auto;
+    border: solid 0px;
+    border-right: solid 1px #27313D;
+  }
+
+  .md .after {
+    display: inline-block;
+    width: 50%;
+    height: 100%;
+    overflow-y: auto;
+    background: #F7F7F7;
+    padding: 20px;
+    box-sizing: border-box;
+    letter-spacing: 2px;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+  }
+
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+  {
+    opacity: 0
+  }
 </style>
